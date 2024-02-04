e4a88de0bb9874bc25ae20423557a46b8b348e23
jffhff <jffhff+github@thehoffmanns.net>
2024-02-04 12:39:59 -0500

    Revert web templating

--- src/web_interface.cpp
+++ src/web_interface.cpp
@@ -65,41 +65,6 @@ void web_request_root(AsyncWebServerRequest *request)
   request->send(200, "text/plain", s);
 }
 
-const int MAX_TEMPLATE_VARIABLES = 10;
-String *template_variable_names[MAX_TEMPLATE_VARIABLES];
-String *template_variable_values[MAX_TEMPLATE_VARIABLES];
-int next_free_slot = 0;
-
-void register_template_variable(const char *name, const char *value)
-{
-  if (next_free_slot < MAX_TEMPLATE_VARIABLES)
-  {
-    template_variable_names[next_free_slot] = new String(name);
-    template_variable_values[next_free_slot] = new String(value);
-    next_free_slot++;
-  }
-  else
-  {
-    Log.println("Maximum template variables reached. Can not register additional variable");
-  }
-}
-
-String templateProcessor(const String &var)
-{
-  if (var == "HELLO_FROM_TEMPLATE")
-    return F("Hello world!");
-
-  for (int variable_index = 0; variable_index < next_free_slot; variable_index++)
-  {
-    if (var == *template_variable_names[variable_index])
-    {
-      return F(template_variable_values[variable_index]);
-    }
-  }
-
-  return String();
-}
-
 void web_interface_setup()
 {
   /* Respond to MDNS queries for artnet.local, or configured hostname.local */
@@ -109,20 +74,18 @@ void web_interface_setup()
   {
     Log.println("Using MDNS hostname artnet.local");
     mdns.begin(WiFi.localIP(), "artnet");
-    register_template_variable("WEBSOCKET_URL", "ws://artnet.local:80/ws");
   }
   else
   {
     Log.println("Using MDNS hostname " + web_interface_param_mdns_hostname);
     mdns.begin(WiFi.localIP(), web_interface_param_mdns_hostname.c_str());
-    register_template_variable("WEBSOCKET_URL", ("ws://" + web_interface_param_mdns_hostname + ".local:80/ws").c_str());
   }
 
   server.on("/", HTTP_GET, web_request_root);
   // Serve files in directory "/www/" when request url starts with "/"
   // Request to the root or none existing files will try to server the default
   // file name "index.htm" if exists
-  server.serveStatic("/", SPIFFS, "/www/"); //.setTemplateProcessor(templateProcessor);
+  server.serveStatic("/", SPIFFS, "/www/");
   server.onNotFound(web_request_not_found);
 
   ws.onEvent(onWsEvent);
